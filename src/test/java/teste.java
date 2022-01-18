1bbbad87ab339755182651fb4c21aca740a1af2e
Bruno <bruno.noberto@hotmail.com>
2022-01-18 07:12:23 -0300

    ola mundo

--- src/test/java/teste.java
+++ src/test/java/teste.java
@@ -1,4 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
 
 public class teste {
 
+	public static void main(String[] args) {
+		
+		Response request = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+		
+		System.out.println(request.getBody().asString());
+	}
 }
