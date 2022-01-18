35345b852f7c7b4246338b28747bb76328d72041
Bruno <bruno.noberto@hotmail.com>
2022-01-18 08:33:02 -0300

    teste com hamcrest

--- src/test/java/testeJunit.java
+++ src/test/java/testeJunit.java
@@ -1,8 +1,19 @@
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.text.StringContainsInOrder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import groovyjarjarantlr4.v4.parse.ANTLRParser.notSet_return;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -54,6 +65,25 @@ public class testeJunit {
 			.get("https://restapi.wcaquino.me/ola")
 		.then()
 			.statusCode(200);
+	}
+	
+	@Test
+	public void testeHamcrest() {
+		
+		// primeiro parametro vem da aplicacao e o segundo é o do codigo
+		Assert.assertThat("Maria", Matchers.is("Maria"));
+		Assert.assertThat(128, Matchers.is(128));
+		Assert.assertThat(128, Matchers.isA(Integer.class));
+		Assert.assertThat(128d, Matchers.greaterThan(120d)); // maior que
+		
+		// Utilizando o import statico em assert e matcher
+		List<Integer> impares = Arrays.asList(1, 3, 5, 7);
+		assertThat(impares, hasSize(4));
+		assertThat(impares, contains(1, 3, 5, 7));
+		assertThat(impares, containsInAnyOrder(5, 3, 7, 1));
+		assertThat(impares, Matchers.hasItem(1));
+		assertThat(impares, Matchers.hasItems(3, 5));
+		
 	}
 
 }
